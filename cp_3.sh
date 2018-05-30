f597dcfcb092b7482e1c5def70d710107cd1a607
orangetangerine <263507065@qq.com>
2018-05-30 14:27:45 +0800

    update

---
+++ cp_3.sh
@@ -0,0 +1,13 @@
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp -r oh-my-zsh/ ~/.oh-my-zsh;
+
+# alias
+cp aliases ~/.aliasws
+
+# zshrc
+cp zshrc ~/.zshrc
+
+# vim
+cp vimrc ~/.vimrc
---
+++ dev_enviroment_4.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: dev_enviroment_4.sh
+# Author: Orange
+#
+# Created Time: 2018-05-28
+#########################################################################
+
+#!/bin/bash
+git config --global user.name "orangetangerine"
+git config --global user.email "263507065@qq.com"
